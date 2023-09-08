cfd9e8d02320418267e75ca2e0a4e0293b760a3c
Thomas Butler <tbutler0249@gmail.com>
2023-09-08 21:17:13 +0100

    Restore nav bar collapse feature

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ let navBar = `
       <a class="navbar-brand" href="#">
           <img src="images/favicon-color.png" alt="Bootstrap" width="30" height="24">
       </a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="index.html">Home</a>
             </li>
@@ -26,6 +30,7 @@ let navBar = `
                 <a class="nav-link" href="aspeck1/index.html">Andrew Peck</a>
             </li>
         </ul>
+	  </div>
     </div>
 </nav>
 <div class="nav-spacer"></div>`;
